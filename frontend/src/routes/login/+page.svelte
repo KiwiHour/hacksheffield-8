709674527c9d4d4d88b9cc23bb6951de6c19d00f
Matthew Basford <matthew.teabear.basford@gmail.com>
2023-11-11 19:34:29 +0000

    more css changes on login page

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -6,9 +6,9 @@
 </script>
 
 <div id="login-wrapper">
-    <input type="email" placeholder="Email" bind:value={email} />
-    <input type="password" placeholder="Password" bind:value={password} />
-    <input type="button" value="login" on:click={() => "do shit"}>
+    <input class="text-input" type="email" placeholder="Email" bind:value={email} />
+    <input class="text-input" type="password" placeholder="Password" bind:value={password} />
+    <input id="login-button" type="button" value="login" on:click={() => "do shit"}>
 </div>
 
 <div id="register">
@@ -21,8 +21,25 @@
         width: 50%;
         display: flex;
         flex-flow: column wrap;
+        align-items: center;
         justify-content: space-between;
         margin: auto auto;
+
+        & .text-input {
+            width: 100%
+        }
+
+        & #login-button {
+            width: 50%;
+        }
+    }
+
+
+    #register {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
     }
 
 </style>
