6621240ef85ed3bae201c152242adbd97e649980
WillJeynes <william.jeynes@hotmail.com>
2023-11-11 12:53:57 +0000

    update update script

--- api/update.sh
+++ api/update.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-cd /home/helloworld/api/HackSheffield9
+cd /home/helloworld/hacksheffield-8-main
 killall dotnet
 git fetch --all
 git reset --hard origin/main
 curl localhost:2019/load -H "Content-Type: application/json" -d @caddy_config.json
-chmod a+rwx ~/api/HackSheffield9/data.db
+chmod a+rwx /home/helloworld/hacksheffield-8-main/api/data.db
+cd api
 dotnet publish --configuration Release HackSheffield.csproj 
-dotnet /home/helloworld/api/HackSheffield9/bin/Release/net7.0/HackSheffield.dll &
-disown
+dotnet /home/helloworld/api/hacksheffield-8-main/bin/Release/net7.0/HackSheffield.dll &
+
+cd ..
+cd frontent
+npm run build
