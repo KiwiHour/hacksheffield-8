eee41df0c6a6e529ee67e52dd1453bd9c81b9584
Ben Cannon <bcannon1@sheffield.ac.uk>
2023-11-11 17:44:54 +0000

    stuff

--- api/Models/GetKey.cs
+++ api/Models/GetKey.cs
@@ -1,35 +1,10 @@
+using System.Drawing.Printing;
+using Azure.AI.OpenAI;
+
 public class GetKey
 {
     public static string getKey()
     {
-        string line = "";
-        try
-        {
-            //Pass the file path and file name to the StreamReader constructor
-            StreamReader sr = new StreamReader("../../openai.txt");
-            //Read the first line of text
-            line = sr.ReadLine();
-            //Continue to read until you reach end of file
-            while (line != null)
-            {
-                //write the line to console window
-                Console.WriteLine(line);
-                //Read the next line
-                line = sr.ReadLine();
-            }
-            //close the file
-            sr.Close();
-            Console.ReadLine();
-        }
-        catch(Exception e)
-        {
-            Console.WriteLine("Exception: " + e.Message);
-        }
-        finally
-        {
-            Console.WriteLine("Executing finally block.");
-        }
-
-        return line;
+        return "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";
     }
 }
