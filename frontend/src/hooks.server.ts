a102251faacdc63a248593df59ebd51b48a9ddd3
Matthew Basford <matthew.teabear.basford@gmail.com>
2023-11-11 14:20:49 +0000

    fixed hooks.server.ts spelling error in filename

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -2,9 +2,10 @@ import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 
+    console.log("ok")
     let apiKey = event.cookies.get("apiKey") as string;
 
-    if (!apiKey)
+    if (!apiKey && event.url.pathname != "/login")
         throw redirect(302, "/login");
 
 	return await resolve(event);
