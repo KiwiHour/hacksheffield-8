adc9179f93c7b512330f79290fa131c4e1852d88
Ogladian <ayuubumar1234@gmail.com>
2023-11-11 14:43:56 +0000

    Layout Boilerplate

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+
+    export let data: LayoutData;
+</script>
+
+<div id = "navbar"></div>
+
+<main>
+    <slot />
+</main>
+
+<style>
+    h1 {
+
+        p {
+
+        }
+    }
+</style>
+
