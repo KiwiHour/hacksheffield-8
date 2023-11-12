5da62bf1b5b2299af46b4e37f95db2f9d7c0ae2e
WillJeynes <william.jeynes@hotmail.com>
2023-11-12 01:56:53 +0000

    l

--- api/Models/GetKey.cs
+++ api/Models/GetKey.cs
@@ -1,10 +1,1 @@
-using System.Drawing.Printing;
-using Azure.AI.OpenAI;
-
-public class GetKey
-{
-    public static string getKey()
-    {
-        return "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";
-    }
-}
+public class GetKey{public static string getKey(){return "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}
