00ae156d3a3029c9798f1026a96a36f3d5be6590
WillJeynes <william.jeynes@hotmail.com>
2023-11-12 02:48:36 +0000

    p

--- api/update.sh
+++ api/update.sh
@@ -6,16 +6,19 @@ res="public class GetKey{public static string getKey(){return \"$value\";}}"
 
 # echo "hello" > qwe.txt
 echo $res
-rm -rf Models/GetKey.cs
-rm -rf openai.txt
-echo $res > Models/GetKey.cs
-echo $value > openai.txt
 
 
 cd /home/helloworld/hacksheffield-8-main
 killall dotnet
 git fetch --all
 git reset --hard origin/main
+
+rm -rf Models/GetKey.cs
+rm -rf openai.txt
+echo $res > Models/GetKey.cs
+echo $value > openai.txt
+
+
 curl localhost:2019/load -H "Content-Type: application/json" -d @caddy_config.json
 chmod a+rwx /home/helloworld/hacksheffield-8-main/api/data.db
 cd api
